66049afcd8a29cac58387d335cb919dad5c9595b
Marcio Antonio Alves Pereira <146758595+MAAPereira-github@users.noreply.github.com>
2024-06-03 09:33:27 -0300

    Update arquivos.sql

--- arquivos.sql
+++ arquivos.sql
@@ -1,9 +1,1 @@
-select --sum(decode(sinlan, '+', qtdhor, 0)), sum(decode(sinlan, '-', qtdhor, 0)),
-   max(datlan) dta_lanc,
-   trunc(sum(decode(sinlan, '+', qtdhor, '-', -qtdhor, 0)) / 60) as Hora,
-   ((sum(decode(sinlan, '+', qtdhor, '-', -qtdhor, 0)) / 60) -  
-trunc(sum(decode(sinlan, '+', qtdhor, '-', -qtdhor, 0)) / 60)) * 60 as Minuto
-  from R011LAN
-    where numcad = 900506 -- Informar o c�digo do funcion�rio. Geralmente, � 7000 + N�mero do Crach�
-      and numemp = 1   
-      and datlan < trunc(sysdate - 10);
+teste
